b5d6133b429521db5c9f544cb4401aace37976e9
unknown <amaramaswitha@gmail.com>
2025-12-06 14:25:00 +0530

    files added

---
+++ addition.java
@@ -0,0 +1,15 @@
+public class addition {
+
+    public static void main(String[] args) {
+       
+        int sum = 0;
+
+        
+        for (int i = 1; i <= 10; i++) {
+            sum += i; 
+        }
+
+       
+        System.out.println("The sum of the first 10 numbers is: " + sum);
+    }
+}

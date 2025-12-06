5cd3b9cc99c4316a86693b442ef9ff62e08c17ed
unknown <amaramaswitha@gmail.com>
2025-12-06 14:38:49 +0530

    files added

---
+++ sumofodd.java
@@ -0,0 +1,16 @@
+public class sumofodd {
+
+    public static void main(String[] args) {
+       
+        int sum = 0;
+
+        
+        int number = 1; 
+        for (int i = 1; i <= 10; i++) {
+            sum += number; 
+            number += 2; 
+        }
+
+        System.out.println("The sum of the first 10 odd numbers is: " + sum);
+    }
+}
